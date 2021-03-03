ca29b1452e9085006952f5103d5bae79ab446230
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2021-03-03 18:32:55 +0000

    Sync LeetCode submission - Missing Number (cpp)

---
+++ my-folder/problems/missing_number/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+
+        int sum=0;
+        
+        for(int i=1;i<=nums.size();++i){
+            sum+=i;
+            sum-=nums[i-1];
+        }
+        return sum;
+    }
+};
