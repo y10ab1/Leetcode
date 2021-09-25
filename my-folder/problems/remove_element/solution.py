db10b9a5b31050355d03690ed21ac26828f56624
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2021-09-25 07:51:12 +0000

    Sync LeetCode submission - Remove Element (python)

--- my-folder/problems/remove_element/solution.py
+++ my-folder/problems/remove_element/solution.py
@@ -1,12 +1,14 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        OK = 0
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
         c = 0
-        
         for i in range(len(nums)):
-            if nums[i] != val:
-                nums[c] = nums[i]
-                OK+=1
+            if nums[i]!=val:
+                nums[i], nums[c] = nums[c], nums[i]
                 c+=1
-        return OK
-                
+        return c
+        
