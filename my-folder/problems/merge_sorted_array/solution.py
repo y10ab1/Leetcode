8cf475bfcd17ccb8ce399633351e67f3543ebf9b
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2021-10-03 08:42:42 +0000

    Sync LeetCode submission - Merge Sorted Array (python3)

--- my-folder/problems/merge_sorted_array/solution.py
+++ my-folder/problems/merge_sorted_array/solution.py
@@ -10,27 +10,26 @@ class Solution:
         for i in range(n):
             nums1.insert(0,nums1.pop())
         
-        a, b, c = 0, 0, 0
+        id1 = 0
+        id2 = 0
+        idx = 0
         
-        
-        while c < m+n :
-            if a<m and b<n:
-                if nums1[a+n] < nums2[b]:
-                    nums1[c] = nums1[a+n]
-                    a+=1
-                    c+=1
+        while idx < m+n:
+            if id1<m and id2<n:
+                if nums1[id1+n] < nums2[id2]:
+                    nums1[idx] = nums1[id1+n]
+                    idx+=1
+                    id1+=1
                 else:
-                    nums1[c] = nums2[b]
-                    b+=1
-                    c+=1
-            elif a==m and b<n:
-                nums1[c] = nums2[b]
-                b+=1
-                c+=1
-            elif a<m and b==n:
-                nums1[c] = nums1[a+n]
-                a+=1
-                c+=1
+                    nums1[idx] = nums2[id2]
+                    idx+=1
+                    id2+=1
+            elif id2>=n:
+                nums1[idx:] = nums1[id1+n:]
+                return
+            elif id1>=m:
+                nums1[idx:] = nums2[id2:]
+                return
         
             
             
