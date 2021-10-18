9c31b7be350ba492a02bd793bd7e982eb3099021
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2021-10-18 19:21:51 +0000

    Sync LeetCode submission - Single Number (python3)

--- my-folder/problems/single_number/solution.py
+++ my-folder/problems/single_number/solution.py
@@ -1,6 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans = 0
-        for n in nums:
-            ans ^= n
+        
+        ans = nums[0]
+        for i in range(len(nums)-1):
+            ans ^= nums[i+1]
         return ans
