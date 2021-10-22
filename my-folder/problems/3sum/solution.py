a6d20d11c5a62ff42ce68baa73e03cc7bf599f7d
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2021-10-22 13:23:40 +0000

    Sync LeetCode submission - 3Sum (python3)

--- my-folder/problems/3sum/solution.py
+++ my-folder/problems/3sum/solution.py
@@ -3,25 +3,32 @@ class Solution:
         if len(nums) <3:
             return []
         nums = sorted(nums)
+        print(nums)
+        d = {}
         
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
         ans = []
-        
         for i in range(len(nums)-2):
-            if (i>0 and nums[i]==nums[i-1]): continue
-                
-            l = i+1; r = len(nums) -1 
-            while l < r:
-                s = nums[i]+nums[l]+nums[r]
-                if s > 0:
-                    r-=1
-                elif s < 0:
-                    l+=1
-                else:
-                    ans.append([nums[i],nums[l],nums[r]])
-                    while l+1 < r and nums[l] == nums[l+1]:
-                        l+=1
-                    while r-1 > l and nums[r] == nums[r-1]:
-                        r-=1
-                    l+=1;r-=1
+            j = i+1
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+            while j < len(nums):
+                if i!=j-1 and nums[j] == nums[j-1]:
+                    j+=1
+                    continue
+                s = 0 - nums[i] - nums[j]
+                if s in d and s>=nums[j] and nums[j]>=nums[i]:
+                    if s == 0 and d[s]>=3:
+                        ans.append([nums[i],nums[j],s])
+                    elif s!=0 and (s==nums[i] or s==nums[j]) and d[s]>=2:
+                        ans.append([nums[i],nums[j],s])
+                    elif s!=0 and (s!=nums[i] and s!=nums[j]):
+                        ans.append([nums[i],nums[j],s])
+                j+=1    
         return ans
+                    
         
