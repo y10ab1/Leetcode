5be68fb26f4871651a5394ce317996ba19f60891
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2021-10-23 08:20:00 +0000

    Sync LeetCode submission - Longest Palindromic Substring (python3)

--- my-folder/problems/longest_palindromic_substring/solution.py
+++ my-folder/problems/longest_palindromic_substring/solution.py
@@ -1,15 +1,24 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
+    
+    def longestPalindrome(self, s):
         res = ""
         for i in range(len(s)):
-            res = max(self.longestSubstring(s,i,i), self.longestSubstring(s,i,i+1), res, key=len)
+            # odd case, like "aba"
+            tmp = self.helper(s, i, i)
+            if len(tmp) > len(res):
+                res = tmp
+            # even case, like "abba"
+            tmp = self.helper(s, i, i+1)
+            if len(tmp) > len(res):
+                res = tmp
         return res
-            
-    def longestSubstring(self, s: str, l: int, r: int) -> str:
-        longestStr = ""
-        while l >= 0 and r < len(s):
-            if s[l] == s[r]:
-                l, r = l-1, r+1
-            else:
-                break
-        return s[l+1: r]
+
+    # get the longest palindrome, l, r are the middle indexes   
+    # from inner to outer
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1; r += 1
+        return s[l+1:r]
+    
+    
+    
