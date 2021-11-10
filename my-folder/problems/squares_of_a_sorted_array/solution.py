716781d88cdcd947b99e3bdb9b9b5724a94079e4
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2021-11-10 19:12:53 +0000

    Sync LeetCode submission - Squares of a Sorted Array (python3)

--- my-folder/problems/squares_of_a_sorted_array/solution.py
+++ my-folder/problems/squares_of_a_sorted_array/solution.py
@@ -1,41 +1,31 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        
-        idx = find_cross_zero_id(nums)
-        
-        for i in range(len(nums)):
-            nums[i] = nums[i]**2
-        
-        back_nums = nums[0:idx]
-        back_nums = back_nums[::-1]
-        if idx < len(nums):
-            nums = nums[idx:]
-        else:
-            nums=[]
-
-        
         ans = []
-        a=0
-        b=0
-        while a!=len(nums) and b!=len(back_nums):
-            if nums[a] < back_nums[b]:
-                ans.append(nums[a])
-                a+=1
-            else:
-                ans.append(back_nums[b])
-                b+=1
-        if len(nums) - a > len(back_nums)-b:
-            ans += nums[a:]
-        else:
-            ans += back_nums[b:]
+        p, n = [], []
+        for i in range(len(nums)):
+            if nums[i] >= 0:
+                p = nums[i:]
+                n = nums[:i]
+                break
+        if len(p) == 0 and len(n) == 0:
+            n = nums
+        n = n[::-1]
         
+        for i in range(len(p)):
+            p[i] = p[i]**2
+        for i in range(len(n)):
+            n[i] = n[i]**2
+        idp, idn = 0, 0
+        while idp < len(p) and idn < len(n):
+            if p[idp] < n[idn]:
+                ans.append(p[idp])
+                idp+=1
+            else:
+                ans.append(n[idn])
+                idn+=1
+        
+        if idp >= len(p):
+            ans+=n[idn:]
+        else:
+            ans+=p[idp:]
         return ans
-            
-def find_cross_zero_id(nums):
-    if nums[0] >= 0:
-        return 0
-    elif nums[-1] < 0:
-        return len(nums)
-    for i in range(len(nums)-1):
-        if nums[i] < 0 and nums[i+1] >= 0:
-            return i+1
