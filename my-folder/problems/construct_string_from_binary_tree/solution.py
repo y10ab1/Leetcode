7d14cc8635d46291c7414cfbb8fb0fed0c0ae87c
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-09-07 11:48:42 +0000

    Sync LeetCode submission - Construct String from Binary Tree (python3)

---
+++ my-folder/problems/construct_string_from_binary_tree/solution.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        ans = []
+        
+        def dfs(node: TreeNode):
+            
+            
+            ans.append(f"({node.val}")
+            if not node.left and node.right:
+                ans.append("()")
+                dfs(node.right)
+            else:
+                if node.left:
+                    dfs(node.left)
+                if node.right:
+                    dfs(node.right)
+            ans.append(")")
+        
+        dfs(root)
+        return "".join(ans)[1:-1]
