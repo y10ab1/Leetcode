a32d877a3b7db0a6efd7e6a4057947f57642e0ea
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-09-12 03:07:33 +0800

    Time: 1918 ms (25.63%), Space: 29.1 MB (78.89%) - LeetHub

--- 2393-count-strictly-increasing-subarrays/2393-count-strictly-increasing-subarrays.py
+++ 2393-count-strictly-increasing-subarrays/2393-count-strictly-increasing-subarrays.py
@@ -1,10 +1,11 @@
 class Solution:
     def countSubarrays(self, nums: List[int]) -> int:
+        
         ans = len(nums)
         l = 0
-        for r in range(l+1, len(nums)):
+        for r in range(1, len(nums)):
             if nums[r] > nums[r-1]:
                 ans += r-l
             else:
-                l=r
+                l,r=r,r+1
         return ans
