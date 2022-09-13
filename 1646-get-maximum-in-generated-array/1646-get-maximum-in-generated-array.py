b2611b4d088e6006d97a3688b710f58a412246a4
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-09-13 21:17:14 +0800

    Time: 65 ms (11.54%), Space: 13.8 MB (64.34%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
@@ -1,9 +1,6 @@
 class Solution:
     def getMaximumGenerated(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
+
         
         dp = [0,1]
         for i in range(2,n+1):
@@ -11,4 +8,4 @@ class Solution:
                 dp.append(dp[i//2])
             else:
                 dp.append(dp[i//2]+dp[i//2+1])
-        return max(dp)
+        return max(dp[:n+1])
