95297e7d3504370cfbbee4b38c8a5c5b84f737c0
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-09-13 17:30:18 +0000

    Sync LeetCode submission - Meeting Rooms II (python3)

---
+++ my-folder/problems/meeting_rooms_ii/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        startheap = intervals
+        endheap = []
+        cnt=1
+        
+        heapq.heapify(startheap)
+            
+        while startheap:
+            ss,se = heapq.heappop(startheap)
+            if endheap:
+                ee,es = endheap[0]
+                if ss>=ee:
+                    heapq.heappop(endheap)
+                else:
+                    cnt+=1
+            heapq.heappush(endheap, (se,ss))
+                
+
+        return cnt
+                
