ce29ca2617a157403f2af40c2624bc2d6d1f91c1
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-09-14 09:11:37 +0000

    Sync LeetCode submission - Pseudo-Palindromic Paths in a Binary Tree (python3)

---
+++ my-folder/problems/pseudo-palindromic_paths_in_a_binary_tree/solution.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        bitmask = 0
+        self.ans = 0
+        
+        
+        def dfs(node, bitmask):
+            bit = 1<<(node.val - 1)
+            bitmask^=bit
+            
+            if node.left: dfs(node.left, bitmask)
+            if node.right: dfs(node.right, bitmask)
+            
+            if node.left == node.right:
+                if not bitmask ^ (bitmask & -bitmask):
+                    self.ans +=1
+                    
+        dfs(root, bitmask)
+        return self.ans
+            
+            
+            
