f4dfc75de9acb5ffe8921d10fd75c4696a3277a2
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-09-16 03:26:42 +0800

    Time: 187 ms (24.68%), Space: 16 MB (60.96%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,9 +1,10 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         d = collections.Counter(nums)
-        ans = 0
+        
         keys = sorted(d.keys())
         pre,cur = 0, d[keys[0]]*keys[0]
+        
         for i in range(1,len(keys)):
             k = keys[i]
             if k-1 == keys[i-1]:
