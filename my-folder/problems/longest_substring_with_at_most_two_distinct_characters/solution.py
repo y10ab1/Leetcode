7ee65d9dc26a3849a5c487996b28d55b3e43c8c4
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-09-16 15:47:40 +0000

    Sync LeetCode submission - Longest Substring with At Most Two Distinct Characters (python3)

---
+++ my-folder/problems/longest_substring_with_at_most_two_distinct_characters/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+        l,r=0,0
+        maxlen=0
+        char_set = defaultdict(int)
+        while r<len(s):
+            char_set[s[r]]+=1
+            
+            if len(char_set) <= 2:
+                maxlen = max(maxlen,r-l+1)
+            else:
+                char_set[s[l]]-=1
+                if char_set[s[l]] <= 0:
+                    del char_set[s[l]]
+                l+=1
+            r+=1
+        return maxlen
+    
+        # time: O(n)
+        # space: O(n)
