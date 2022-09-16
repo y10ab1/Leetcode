98069b07b9b3399d6366fc763a20d2262255ed06
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-09-16 14:25:26 +0000

    Sync LeetCode submission - Maximum Score from Performing Multiplication Operations (python3)

--- my-folder/problems/maximum_score_from_performing_multiplication_operations/solution.py
+++ my-folder/problems/maximum_score_from_performing_multiplication_operations/solution.py
@@ -1,14 +1,15 @@
 class Solution:
-    def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
-        m = len(multipliers)
-        n = len(nums)
-        
+    def maximumScore(self, nums: List[int], mul: List[int]) -> int:
+        n, m = len(nums), len(mul)
         dp = [[0]*(m+1) for _ in range(m+1)]
         
-        for i in range(m-1,-1,-1):
-            for j in range(i,-1,-1):
-                mul = multipliers[i]
-                l_n = nums[j]
-                r_n = nums[n-1-(i-j)]
-                dp[i][j] = max(l_n*mul+dp[i+1][j+1], r_n*mul+dp[i+1][j])
+        # l -> left index of nums, r -> right index of nums
+        # k -> index of mul
+        # dp[k][l] = max(nums[l]*mul[k]+dp[l+1][k+1], nums[r]*mul[k]+dp[l][k+1])
+        
+        for k in range(m-1,-1,-1):
+            for l in range(k,-1,-1):
+                r = n-1-(k-l)
+                dp[k][l] = max(nums[l]*mul[k]+dp[k+1][l+1], nums[r]*mul[k]+dp[k+1][l])
+        
         return dp[0][0]
