4a9459d05e4c096ed051eca126172cb9481691ef
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-09-18 02:51:46 +0800

    Time: 1663 ms (16.23%), Space: 27.7 MB (97.70%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -2,10 +2,10 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         maxsum,cursum = -10001,-10001
         for n in nums:
-            if 0 > cursum:
-                cursum = n
-            else:
+            if cursum>=0:
                 cursum += n
+            else:
+                cursum = n
             maxsum = max(cursum, maxsum)
         return maxsum
             
