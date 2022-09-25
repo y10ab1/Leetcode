30b6f5361b7918495011c47ec17dabd95d84d156
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-09-25 20:45:22 +0800

    Time: 8171 ms (5.13%), Space: 180.3 MB (5.77%) - LeetHub

---
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.py
@@ -0,0 +1,17 @@
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        d = defaultdict(list)
+        n = len(nums)
+        ans = 0
+        for i in range(n):
+            for j in range(i+1,n):
+                d[nums[i]*nums[j]].append(set([nums[i],nums[j]]))
+        
+        for k in d.keys():
+            if len(d[k]) > 1:
+                print(d[k])
+                for i in range(len(d[k])):
+                    for j in range(i+1,len(d[k])):
+                        if len(d[k][i] & d[k][j]) == 0:
+                            ans+=8
+        return ans
