204c52f3af9d2f2e274c2d5a01f9ea229c764f19
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-10-02 09:34:30 +0800

    Time: 710 ms (59.36%), Space: 14.7 MB (64.02%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -4,10 +4,11 @@ class Solution:
         #dp[i][j] = dp[i-1]
         
         dp = [[0]*(1000+1) for _ in range(n+1)]
+        premin=0
         for i in range(1,k+1):
             dp[1][i]=1
         for i in range(2,n+1):
-            for j in range(1,target+1):
+            for j in range(min(dp[i-1]),target+1):
                 for kk in range(1,k+1):
                     dp[i][j] += dp[i-1][j-kk]
         return dp[n][target] %(10**9+7)
