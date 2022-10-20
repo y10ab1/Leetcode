81c1ad180382a156f4258c4c8d18859e74c7fd33
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-10-20 13:25:52 +0000

    Sync LeetCode submission - Integer to Roman (python3)

---
+++ my-folder/problems/integer_to_roman/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        d = {1:'I'
+            ,4:'IV'
+            ,5:'V'
+            ,9:'IX'
+            ,10:'X'
+            ,40:'XL'
+            ,50:'L'
+            ,90:'XC'
+            ,100:'C'
+            ,400:'CD'
+            ,500:'D'
+            ,900:'CM'
+            ,1000:'M'}
+        ans = []
+        l = list(d.keys())[::-1]
+        for i in l:
+            ans.append((num//i)*d[i])
+            num%=i
+        return "".join(ans)
+            
