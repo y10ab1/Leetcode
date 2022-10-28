5aab1f2f4a4f6ef9ea77674ae8e98082fa08412b
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-10-28 06:17:08 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- my-folder/problems/group_anagrams/solution.py
+++ my-folder/problems/group_anagrams/solution.py
@@ -1,32 +1,8 @@
 class Solution:
-    
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = {}
-        for st in strs:
-            d = {}
-            for i in range(97,123):
-                d[chr(i)] = 0
-            for s in st:
-                d[s]+=1
-            A = ''
-            for dk,dv in d.items():
-                A += dk+f'{dv}'
-            if not ans.get(A):
-                ans[A] = [st]
-            else:
-                ans[A] += [st]
-           
-        return ans.values()
-    '''
-    def groupAnagrams(self, strs):
-        ans = collections.defaultdict(list)
+        d=defaultdict(list)
         for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-            ans[tuple(count)].append(s)
-        return ans.values()
-                    
-    '''
-                    
-                    
+            d[tuple(sorted(Counter(s).items()))].append(s)
+        return d.values()
+    
+        # O(len(strs)*100log(100))
