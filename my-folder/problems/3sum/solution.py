80733f2e0d9c0f583c8f61e3a4462639d96e17f0
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-12-12 13:03:28 +0000

    Sync LeetCode submission - 3Sum (python3)

--- my-folder/problems/3sum/solution.py
+++ my-folder/problems/3sum/solution.py
@@ -1,34 +1,20 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) <3:
-            return []
-        nums = sorted(nums)
-        print(nums)
-        d = {}
+        d = Counter(nums)
         
-        for i in nums:
-            if i in d:
-                d[i]+=1
+        nums = []
+        for k,v in d.items():
+            if v >= 3:
+                nums += [k for _ in range(3)]
             else:
-                d[i]=1
-        ans = []
-        for i in range(len(nums)-2):
-            j = i+1
-            if i>0 and nums[i] == nums[i-1]:
-                continue
-            while j < len(nums):
-                if i!=j-1 and nums[j] == nums[j-1]:
-                    j+=1
-                    continue
-                s = 0 - nums[i] - nums[j]
-                if s in d and s>=nums[j] and nums[j]>=nums[i]:
-                    if s == 0 and d[s]>=3:
-                        ans.append([nums[i],nums[j],s])
-                    elif s!=0 and (s==nums[i] or s==nums[j]) and d[s]>=2:
-                        ans.append([nums[i],nums[j],s])
-                    elif s!=0 and (s!=nums[i] and s!=nums[j]):
-                        ans.append([nums[i],nums[j],s])
-                j+=1    
-        return ans
-                    
-        
+                nums += [k for _ in range(v)]
+        ans = set()
+        for i in range(len(nums)-1):
+            s = {}
+            for j in range(i+1,len(nums)):
+                if nums[j] in s:
+                    ans.add(tuple(sorted(s[nums[j]]+[nums[j]])))
+                else:
+                    s[-(nums[i]+nums[j])] = [nums[i],nums[j]]
+
+        return list(ans)
