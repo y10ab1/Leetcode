72fcd5dbcdca2d2aaeeea3ca88444aef79b1ea41
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-12-13 17:54:18 +0000

    Sync LeetCode submission - Majority Element (python3)

--- my-folder/problems/majority_element/solution.py
+++ my-folder/problems/majority_element/solution.py
@@ -1,16 +1,3 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        c = {}
-        for i in nums:
-            c[i] = 0
-        for i in nums:
-            c[i]+=1
-        for idx, i in enumerate(c):
-            if idx>0:
-                if Max < c[i]:
-                    Max = c[i]
-                    Maxid = i
-            else:
-                Max = c[i]
-                Maxid = i
-        return Maxid
+        return sorted([(v,k) for k,v in Counter(nums).items()])[-1][1]
