b5d05663961bd92edc0becb396e7270bc66ca410
y10ab1 <94939112+y10ab1@users.noreply.github.com>
2022-12-17 10:36:59 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation (python3)

--- my-folder/problems/evaluate_reverse_polish_notation/solution.py
+++ my-folder/problems/evaluate_reverse_polish_notation/solution.py
@@ -1,38 +1,23 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        numStack = []
+        s = []
         for t in tokens:
-            ans = 0
+
             if t == '+':
-                b = numStack.pop()
-                a = numStack.pop()
-                ans = a+b
-                print(ans)
-                numStack.append(ans)
+                tmp = s.pop()
+                s[-1]+=tmp
             elif t == '-':
-                b = numStack.pop()
-                a = numStack.pop()
-                ans = a-b
-                print(ans)
-                numStack.append(ans)
+                tmp = s.pop()
+                s[-1]-=tmp
             elif t == '*':
-                b = numStack.pop()
-                a = numStack.pop()
-                ans = a*b
-                print(ans)
-                numStack.append(ans)
+                tmp = s.pop()
+                s[-1]*=tmp
             elif t == '/':
-                b = numStack.pop()
-                a = numStack.pop()
-                ans = a/b
-                if ans < 0:
-                    ans = -1*(-1*ans //1)
-                else:
-                    ans = ans // 1
-                
-                numStack.append(ans)
+                tmp = s.pop()
+                s[-1] = int(s[-1]/tmp)
             else:
-                numStack.append(int(t))
-            
-        return int(numStack.pop())
-            
+                s.append(int(t))
+
+        return s[-1]
+
+                
